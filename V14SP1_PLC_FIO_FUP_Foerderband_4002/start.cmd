bd1a50157236e6a9dcc7fdcdaf5f5cb25464d6ce
Kurt Linder <32621408+KurtLinder@users.noreply.github.com>
2018-02-27 11:58:38 +0100

    Ein paar neue Projekte hinzugefügt

--- V14SP1_PLC_FIO_FUP_Foerderband_4002/start.cmd
+++ V14SP1_PLC_FIO_FUP_Foerderband_4002/start.cmd
@@ -1,4 +1,6 @@
 h:
-start h:\TiaPortal_V14\Factory_IO\Factory_IO_Foerderband.factoryio
+del "%localappdata%\Real Games\Factory IO\*.*" /Q /S
+copy h:\TiaPortal_V14\Factory_IO\config.cfg "%localappdata%\Real Games\Factory IO\config.cfg"
 cd h:\TiaPortal_V14\Foerderband
+start h:\TiaPortal_V14\Factory_IO\Factory_IO_Foerderband.factoryio
 start Foerderband.ap14
