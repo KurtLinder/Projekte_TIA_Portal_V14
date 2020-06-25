79d4363f4f9e941d3dd1f7810a0f2c4e35c7bef7
Kurt Linder <kurt.linder@lbsfe1.snv.at>
2020-06-25 15:48:31 +0200

    Newtonsoft.Json.dll, ProjektVerzeichnisse.json und 115 weitere dateien aktualisiert...

---
+++ V14SP1_PLC_Snap7_FUP_LAP__2010__4__Abfuellanlage/start.cmd
@@ -0,0 +1,4 @@
+h:
+cd h:\TiaPortal_V14\LAP_2010_4_Abfuellanlage
+start LAP_2010_4_Abfuellanlage.ap14
+start LAP_2010_4_Abfuellanlage.exe
---
+++ V14SP1_PLC_Snap7_FUP_LAP__2018__4__Niveauregelung/start.cmd
@@ -0,0 +1,4 @@
+h:
+cd h:\TiaPortal_V14\LAP_2018_Niveauregelung
+start LAP_2018_Niveauregelung.ap14
+start LAP_2018_Niveauregelung.exe
